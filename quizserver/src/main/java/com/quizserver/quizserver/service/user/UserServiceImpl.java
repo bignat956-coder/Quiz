84bf7953b489d2f3b25d321e59928c893a4672b3
Godwin007-tech <godwinameh006@gmail.com>
2025-11-05 19:54:07 +0000

    userservimpl

--- quizserver/src/main/java/com/quizserver/quizserver/service/user/UserServiceImpl.java
+++ quizserver/src/main/java/com/quizserver/quizserver/service/user/UserServiceImpl.java
@@ -1,9 +1,22 @@
 package com.quizserver.quizserver.service.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.quizserver.quizserver.entities.User;
+import com.quizserver.quizserver.repository.UserRepository;
 
 @Service
 
 public class UserServiceImpl implements UserService {
 
+@Autowired
+private UserRepository userRepository;
+
+
+private void createAdminUser(){
+
+
+
+}
 }
