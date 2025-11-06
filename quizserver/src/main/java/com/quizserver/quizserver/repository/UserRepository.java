349ddaeb15c685bbcabf437277bf969c305e0203
Godwin007-tech <godwinameh006@gmail.com>
2025-11-06 03:46:04 +0000

    userservimpl

--- quizserver/src/main/java/com/quizserver/quizserver/repository/UserRepository.java
+++ quizserver/src/main/java/com/quizserver/quizserver/repository/UserRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.quizserver.quizserver.entities.User;
+import com.quizserver.quizserver.enums.UserRole;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    List<User> findByRole(UserRole role);
 }
--- quizserver/src/main/java/com/quizserver/quizserver/service/user/UserServiceImpl.java
+++ quizserver/src/main/java/com/quizserver/quizserver/service/user/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.quizserver.quizserver.entities.User;
+import com.quizserver.quizserver.enums.UserRole;
 import com.quizserver.quizserver.repository.UserRepository;
 
 @Service
@@ -14,9 +15,19 @@ public class UserServiceImpl implements UserService {
 private UserRepository userRepository;
 
 
+ 
 private void createAdminUser(){
 
+User optionalUser = userRepository.findByRole(UserRole.ADMIN);
+if(optionalUser == null){
+    User user = new User();
 
+    user.setName("Admin");
+    user.setEmail("admin@gmail.com");
+    user.setRole(UserRole.ADMIN);
+    user.setPassword("admin");
 
+    userRepository.save(user);
+}
 }
 }
