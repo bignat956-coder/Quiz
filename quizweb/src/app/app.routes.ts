5aa33eaf792000ae5c49651943ce07e2a560f2b5
bignat956-coder <bignat956@gmail.com>
2025-11-08 07:06:00 +0000

    sign up component

--- quizweb/src/app/app.routes.ts
+++ quizweb/src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { registerAppScopedDispatcher } from '@angular/core/primitives/event-dispatch';
 import { Routes } from '@angular/router';
+import { Signup } from './modules/auth/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'register', component: Signup}
+
+];
