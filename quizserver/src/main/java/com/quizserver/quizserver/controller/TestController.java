734bd896e20393c4f1b716fc949c0e64cdf55f7a
bignat956-coder <bignat956@gmail.com>
2025-11-11 15:25:57 +0000

    test API

--- quizserver/src/main/java/com/quizserver/quizserver/controller/TestController.java
+++ quizserver/src/main/java/com/quizserver/quizserver/controller/TestController.java
@@ -2,16 +2,32 @@ package com.quizserver.quizserver.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quizserver.quizserver.dto.TestDTO;
 import com.quizserver.quizserver.service.test.TestService;
 
 @RestController
 @RequestMapping("/api/test")
+@CrossOrigin("*")
 public class TestController {
 
     @Autowired
     private TestService testService;
-    
+
+
+    @PostMapping
+    public ResponseEntity<?> createTest(@RequestBody TestDTO dto){
+        try{
+            return new ResponseEntity<>(testService.createTest(dto), HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
