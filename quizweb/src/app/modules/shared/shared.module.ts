dbc638479f54ef29501820ccbb433c6462865814
amehchenemi25-sudo <amehchenemi25@gmail.com>
2025-11-18 15:02:24 +0000

    done

--- quizweb/src/app/modules/shared/shared.module.ts
+++ quizweb/src/app/modules/shared/shared.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { DemoNgZoroAntdModule } from '../../DemoNgZorroAntdModule';
-
-
+// 1. Import the Icon Module and the specific icons
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { UserOutline, LockOutline, MailOutline } from '@ant-design/icons-angular/icons';
+import { NzSelectModule } from 'ng-zorro-antd/select'; 
+const icons = [UserOutline, LockOutline, MailOutline];
 @NgModule({
   declarations: [],
   imports: [
@@ -14,7 +17,9 @@ import { DemoNgZoroAntdModule } from '../../DemoNgZorroAntdModule';
     RouterOutlet,
     RouterLink,
     DemoNgZoroAntdModule,
-		 
+    // 3. Register the icons here using 'forChild'
+    NzIconModule.forChild(icons),
+     NzSelectModule
   ],
   exports: [
     CommonModule,
@@ -23,7 +28,8 @@ import { DemoNgZoroAntdModule } from '../../DemoNgZorroAntdModule';
     RouterOutlet,
     RouterLink,
     DemoNgZoroAntdModule,
-    
+     NzIconModule,
+      NzSelectModule
   
   ],
 })
